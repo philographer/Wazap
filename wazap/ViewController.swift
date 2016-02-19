2d6ec917967bdc949dc78bb83242664a0eb1e83a
yuhogyun <yoohoogun114@naver.com>
2016-02-19 13:06:36 +0900

    Initial Commit

---
+++ wazap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wazap
+//
+//  Created by 유호균 on 2016. 2. 19..
+//  Copyright © 2016년 nexters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
